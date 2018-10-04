aa83b3eb711cf93d553ce76a4ce1cc7212f788c6
aayush <4vedi.aayush@gmail.com>
2018-10-04 14:30:26 +0530

    video/audio functionality added

---
+++ src/styles/colors.js
@@ -0,0 +1,63 @@
+const colors = {
+  white: '#FFFFFF',
+  orange: '#F6C030',
+  gray: 'rgba(155,155,155,1)',
+  transparent: 'transparent',
+  red: 'rgba(190, 0, 4, 1)',
+  primaryColor: '#f9aa33',
+  secondaryColor: '#b2b5b4',
+
+  iconButton: {
+    background: 'transparent',
+    tint: '#FFFFFF',
+    disabled: '#b2b5b4',
+  },
+
+  navigationButton: {
+    enabled: '#f9aa33',
+    disabled: 'rgba(155,155,155,1)',
+  },
+
+  audio: {
+    recording: 'rgba(190, 0, 4, 1)',
+    recordingBackground: '#FFFFFF',
+    playButtonBackground: '#FFFFFF',
+    startRecordingIcon: '#FFFFFF',
+    durationBackground: 'transparent',
+  },
+
+  recordVideo: {
+    recording: '#232f37',
+    recordButtonBorder: '#FFFFFF',
+    durationText: '#FFFFFF',
+    cancelText: '#FFFFFF',
+  },
+
+  navHeader: {
+    tint: '#f9aa33',
+    title: '#000000',
+    background: '#4a6572',
+  },
+  //top
+  tabBar: {
+    border: '#232f37',
+    text: '#4a6572',
+    activeBackground: '#232f37',
+    inactiveBackground: '#FFFFFF',
+  },
+  //bottom
+  tabNavigator: {
+    activeTabIcon: '#F6C030',
+    inactiveTabIcon: '#FFFFFF',
+    background: '#232f37',
+  },
+
+  empty: {
+    title: '#232f37',
+    caption: '#FFFFFF',
+  },
+
+  border: '#232f37',
+};
+
+export default colors;
